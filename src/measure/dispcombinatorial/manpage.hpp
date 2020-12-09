baf44f6ad1b7638e0cabddcdb3e447e0cf6835d4
Benjamin Sommer <benjaminsommer@mailbox.org>
2020-12-09 18:24:47 +0100

    add link in manual of dispnaamad; code format

--- src/measure/dispcombinatorial/manpage.hpp
+++ src/measure/dispcombinatorial/manpage.hpp
@@ -8,61 +8,72 @@
 
 namespace dptk {
 std::string manpage
-  = R"V0G0N(DISPCOMBINATORIAL(1)                         Dispersion Toolkit Manuals                         DISPCOMBINATORIAL(1)
+  = R"V0G0N(DISPCOMBINATORIAL(1)      Dispersion Toolkit Manuals      DISPCOMBINATORIAL(1)
 
 NAME
-       dispcombinatorial - compute dispersion with a combinatorial algorithm (exhaustive search)
+       dispcombinatorial  -  compute dispersion with a combinatorial algorithm
+       (exhaustive search)
 
 SYNOPSIS
-       dispcombinatorial  [--i  FILE]  [--o  FILE]  [--disp]  [--ndisp] [--count-boxes] [--boxes] [--interior-boxes]
-       [--greatest-box] [--silent]
+       dispcombinatorial [--i FILE] [--o FILE]  [--disp]  [--ndisp]  [--count-
+       boxes] [--boxes] [--interior-boxes] [--greatest-box] [--silent]
 
 DESCRIPTION
-       Computes dispersion, n * dispersion, the number of empty boxes, and/or a list of empty boxes of a given point
-       set with cardinality n.
+       Computes  dispersion, n * dispersion, the number of empty boxes, and/or
+       a list of empty boxes of a given point set with cardinality n.
 
-       The measures are written to standard output, or to the file given by --o FILE.
+       The measures are written to standard output, or to the  file  given  by
+       --o FILE.
 
 OPTIONS
        --i FILE, --i=FILE
-              Retrieves  a  point set sequence (P_0, P_1, ..., P_i, ..., P_m) from FILE.  Its absence is substituted
-              by stdin.  The end of a point set P_i, which equals the line #eos, starts the algorithm  to  emit  the
-              requested measure(s) applied on P_i.
+              Retrieves  a  point  set sequence (P_0, P_1, ..., P_i, ..., P_m)
+              from FILE.  Its absence is substituted by stdin.  The end  of  a
+              point  set P_i, which equals the line #eos, starts the algorithm
+              to emit the requested measure(s) applied on P_i.
 
        --o FILE, --o=FILE
-              Redirects  the measures to FILE, opened in overwrite mode (not appending mode).  Without FILE, results
-              are forwarded to stdout.  Errors encountered during the program’s execution are streamed into  stderr,
-              and not into either stdout or FILE.
+              Redirects the measures to FILE, opened in  overwrite  mode  (not
+              appending mode).  Without FILE, results are forwarded to stdout.
+              Errors encountered during the program’s execution  are  streamed
+              into stderr, and not into either stdout or FILE.
 
        --disp Computes dispersion of P_i.
 
        --ndisp
-              Computes dispersion of P_i, multiplied by the cardinality n of P_i.
+              Computes  dispersion  of P_i, multiplied by the cardinality n of
+              P_i.
 
        --count-boxes
-              Counts all empty boxes of P_i, including interiour and exteriour boxes.
+              Counts all empty boxes of P_i, including interiour and exteriour
+              boxes.
 
        --boxes
-              Feeds  all  empty boxes of P_i, including interiour and exteriour boxes, to output.  With the smallest
-              point of a box B being (low_0 low_1), i.e. no other point of B has smaller coordinates, and  with  the
-              upper  point  of B being (up_0 up_1), i.e. no other point of B has greater coordinates, B is formatted
-              as a tuple (low_0 low_1 up_0 up_1) on a distinct line of the output.
+              Feeds  all empty boxes of P_i, including interiour and exteriour
+              boxes, to output.  With the smallest point  of  a  box  B  being
+              (low_0 low_1), i.e. no other point of B has smaller coordinates,
+              and with the upper point of B being (up_0 up_1),  i.e. no  other
+              point  of  B  has greater coordinates, B is formatted as a tuple
+              (low_0 low_1 up_0 up_1) on a distinct line of the output.
 
        --interior-boxes
-              Similar to the option --boxes with the exception of B being restricted to integriour boxes.  A Box  is
-              integriour if it does not intersect with the domain boundary of P_i.
+              Similar to the option --boxes with the exception of B being  re‐
+              stricted  to  integriour  boxes.  A Box is integriour if it does
+              not intersect with the domain boundary of P_i.
 
        --greatest-box
-              Similar  to the option --boxes with the exception of B being the first greatest empty box found during
-              the exhaustive search.
+              Similar to the option --boxes with the exception of B being  the
+              first greatest empty box found during the exhaustive search.
 
        --silent
-              Suppress comments in the output stream, yielding only the computed value.  The  latter  could  be  the
-              point set or its cardinality.
+              Suppress  comments  in the output stream, yielding only the com‐
+              puted value.  The latter could be the point set or its cardinal‐
+              ity.
 
 RETURN FORMAT
    Greatest box
-       A point set of cardinality m with each axis representing a coordinate of a box,
+       A  point  set of cardinality m with each axis representing a coordinate
+       of a box,
 
        point set   low_0   low_1   up_0   up_1
        ────────────────────────────────────────
@@ -71,14 +82,16 @@ RETURN FORMAT
        ...         .       .       .      .
        P_m         .       .       .      .  ,
 
-       where low_i is the smallest coordinate along axis i, and up_i the greatest one.  Notice that the first column
-       is not returned.
+       where low_i is the smallest coordinate  along  axis  i,  and  up_i  the
+       greatest one.  Notice that the first column is not returned.
 
    Boxes
-       A point set sequence, where each point set is a list of the tuple (low_0 low_1 up_0 up_1).
+       A  point  set  sequence,  where  each  point set is a list of the tuple
+       (low_0 low_1 up_0 up_1).
 
    Dispersion, number of boxes
-       A point set of cardinality m with each axis representing a computed measure.
+       A point set of cardinality m with each  axis  representing  a  computed
+       measure.
 
        point set   disp   n*disp   #boxes
        ───────────────────────────────────
@@ -95,6 +108,6 @@ LIMITATION
 AUTHORS
        Benjamin Sommer.
 
-1.0.0                                             December 2, 2020                              DISPCOMBINATORIAL(1)
+1.0.0                          December 2, 2020           DISPCOMBINATORIAL(1)
 )V0G0N";
 }
--- src/measure/dispnaamad/manpage.hpp
+++ src/measure/dispnaamad/manpage.hpp
@@ -7,53 +7,63 @@
 
 namespace dptk {
 std::string manpage
-  = R"V0G0N(DISPNAAMAD(1)                                Dispersion Toolkit Manuals                                DISPNAAMAD(1)
+  = R"V0G0N(DISPNAAMAD(1)             Dispersion Toolkit Manuals             DISPNAAMAD(1)
 
 NAME
        dispnaamad - compute dispersion with algorithm of Naamad et.  al. 1984
 
 SYNOPSIS
-       dispnaamad [--i FILE] [--o FILE] [--disp] [--ndisp] [--count-boxes] [--silent]
+       dispnaamad  [--i  FILE]  [--o  FILE] [--disp] [--ndisp] [--count-boxes]
+       [--silent]
 
 DESCRIPTION
-       Computes dispersion, n * dispersion, and/or the number of RRs considered of a given point set with cardinali‐
-       ty n, according to Naamad et.  al. 1984 (...URL...)
+       Computes dispersion, n * dispersion, and/or the  number  of  restricted
+       rectangles  (RRs)  considered  of a given point set with cardinality n,
+       according          to          Naamad           et.            al. 1984
+       (https://doi.org/10.1016/0166-218X(84)90124-0)
 
        Computational complexity: O(n^2),
 
        Memory complexity: O(n).
 
-       The measures are written to standard output, or to the file given by --o FILE.
+       The  measures  are  written to standard output, or to the file given by
+       --o FILE.
 
 OPTIONS
        --i FILE, --i=FILE
-              Retrieves a point set sequence (P_0, P_1, ..., P_i, ..., P_m) from FILE.  Its absence  is  substituted
-              by  stdin.   The  end of a point set P_i, which equals the line #eos, starts the algorithm to emit the
-              requested measure(s) applied on P_i.
+              Retrieves a point set sequence (P_0, P_1, ...,  P_i,  ...,  P_m)
+              from  FILE.   Its absence is substituted by stdin.  The end of a
+              point set P_i, which equals the line #eos, starts the  algorithm
+              to emit the requested measure(s) applied on P_i.
 
        --o FILE, --o=FILE
-              Redirects the measures to FILE, opened in overwrite mode (not appending mode).  Without FILE,  results
-              are  forwarded to stdout.  Errors encountered during the program’s execution are streamed into stderr,
-              and not into either stdout or FILE.
+              Redirects  the  measures  to FILE, opened in overwrite mode (not
+              appending mode).  Without FILE, results are forwarded to stdout.
+              Errors  encountered  during the program’s execution are streamed
+              into stderr, and not into either stdout or FILE.
 
        --disp Computes dispersion of P_i.
 
        --ndisp
-              Computes dispersion of P_i, multiplied by the cardinality n of P_i.
+              Computes dispersion of P_i, multiplied by the cardinality  n  of
+              P_i.
 
        --count-boxes
-              Counts all empty boxes of P_i, considered as RRs in the publication.
+              Counts  all  empty RRs of P_i, considered as RRs in the publica‐
+              tion.
 
        --silent
-              Suppress comments in the output stream, yielding only the computed value.  The  latter  could  be  the
-              point set or its cardinality.
+              Suppress comments in the output stream, yielding only  the  com‐
+              puted value.  The latter could be the point set or its cardinal‐
+              ity.
 
 RETURN FORMAT
-   Dispersion, number of boxes
-       A point set of cardinality m with each axis representing a computed measure.
+   Dispersion, number of RRs
+       A point set of cardinality m with each  axis  representing  a  computed
+       measure.
 
-       point set   disp   n*disp   #RR boxes
-       ──────────────────────────────────────
+       point set   disp   n*disp   #RRs
+       ─────────────────────────────────
        P_0         .      .        .
        P_1         .      .        .
        ...         .      .        .
@@ -67,6 +77,6 @@ LIMITATION
 AUTHORS
        Benjamin Sommer.
 
-1.0.0                                             December 9, 2020                                     DISPNAAMAD(1)
+1.0.0                          December 9, 2020                  DISPNAAMAD(1)
 )V0G0N";
 }
