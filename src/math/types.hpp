a3dfac122290d3bf2ec4fa58225b61e8c3a51428
Benjamin Sommer <benjaminsommer@mailbox.org>
2021-02-25 08:46:51 +0100

    supporting 128-bit math with GCC: type def, linking

--- src/math/types.hpp
+++ src/math/types.hpp
@@ -1,23 +1,35 @@
 #include <cmath>
-// #include <quadmath.h>
+
+#ifdef __GNUC__
+  #include <quadmath.h>
+#endif
 
 namespace dptk {
 
 typedef float  b32;
 typedef double b64;
-// typedef __float128 b128;
+
+#ifdef __GNUC__
+  typedef __float128 b128;
+#endif
 
 typedef bool               u1;
 typedef unsigned char      u8;
 typedef unsigned short int u16;
 typedef unsigned int       u32;
 typedef unsigned long int  u64;
-// typedef __uint128_t        u128;
+
+#ifdef __GNUC__
+  typedef __uint128_t        u128;
+#endif
 
 typedef char      i8;
 typedef short int i16;
 typedef int       i32;
 typedef long int  i64;
-// typedef __int128_t i128;
+
+#ifdef __GNUC__
+  typedef __int128_t i128;
+#endif
 
 } // namespace dptk
