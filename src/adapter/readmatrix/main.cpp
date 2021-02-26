c905d7ae2c250beabaf80050bacfcf1de653b513
Benjamin Sommer <benjaminsommer@mailbox.org>
2021-02-26 12:25:09 +0100

    supporting Mathematica's matrix format for both read-matrix and write-matrix

--- src/adapter/readmatrix/main.cpp
+++ src/adapter/readmatrix/main.cpp
@@ -3,10 +3,11 @@
 #include "../../io/opointset.hpp"
 #include "../../io/ostream.hpp"
 #include "../../math/pointset.hpp"
+#include <cctype>
 #include <iomanip>
 #include <iostream>
+#include <sstream>
 #include <stdlib.h>
-#include <cctype>
 
 namespace dptk {
 
@@ -23,69 +24,184 @@ struct program_param
   std::ostream* os;
   u1            silent;
   i8            delimiter;
+  i8            point_delimiter;
+  i8            coord_delimiter;
+  i8            point_prefix;
+  i8            point_suffix;
+  i8            set_prefix;
+  i8            set_suffix;
+  i8            comment_prefix;
+  pointset      domain_boundary;
 };
 
 struct problem_param
 {
   pointset       pts;
   program_param* rt;
+
+  u64 input_line;
 };
 
-void read_matrix(std::istream& in, pointset& out)
+enum parser_state : u32
+{
+  outside_set = 0,
+  outside_point,
+  inside_point
+};
+
+u1 known_token(i8 head, const std::string& candidates)
+{
+  for (u64 i = 0; i < candidates.size(); ++i) {
+    if (candidates[i] == head)
+      return true;
+  }
+  return false;
+}
+
+u1 known_token_opt(i8 head, const std::string& candidates)
+{
+  if (candidates.empty())
+    return true;
+  return known_token(head, candidates);
+}
+
+void print_syntax_error(i8 head, problem_param& p)
 {
   std::string ln;
-  i8*         b;
-  i8*         e;
-  i8*         y;
-  i8*         z;
-  u64         d;
-  b64         c;
+  std::getline(*p.rt->is, ln);
 
-  while (std::getline(in, ln) && !in.fail()) {
-    if (ln.empty()) {
+  std::cerr << "syntax error during reading matrix at character '" << head << "' on line "
+            << p.input_line << ". The remain input line is: " << ln << std::endl;
+
+  p.rt->is->setstate(std::ios_base::failbit);
+}
+
+void parse_number(std::string& number, problem_param& p)
+{
+  b64 c;
+
+  c = std::strtod(number.c_str(), nullptr);
+  p.pts.coords.push_back(c);
+  number.clear();
+}
+
+void ensure_dimension(problem_param& p)
+{
+  if (p.pts.dimensions > 0)
+    return;
+  p.pts.dimensions = p.pts.coords.size();
+}
+
+void read_matrix(std::istream& in, problem_param& p)
+{
+  assert(p.rt->point_delimiter != '\0');
+  assert(p.rt->coord_delimiter != '\0');
+  assert(p.rt->point_suffix != '\0' || p.rt->coord_delimiter != p.rt->point_delimiter);
+  assert(p.rt->coord_delimiter != p.rt->point_suffix);
+
+  std::string    ln;
+  i8             head;
+  u64            d;
+  parser_state   state;
+  u1             get_next = true;
+  std::string    number;
+  program_param* rt = p.rt;
+
+  p.pts.dimensions = 0;
+  state            = outside_set;
+  p.input_line     = 0;
+
+  while (in.good()) {
+    if (get_next) {
+      head = in.get();
+
+      // increment line counter
+      if (head == '\n') {
+        ++p.input_line;
+      }
+    }
+    get_next = true;
+
+    // skip comment lines
+    if (head == rt->comment_prefix) {
+      std::getline(in, ln);
+      ++p.input_line;
       continue;
     }
-    if (ln[0] == '#') {
-      std::cerr << "error: input is not allowed to contain lines starting with #"
-                << std::endl;
-      break;
-    }
-    z = (i8*)ln.data();
-    y = &ln[ln.size()];
-    b = z;
-    d = 0;
-    while (b != y) {
-      if (std::isspace(*b) != 0) {
-        ++b;
+
+    // parse point
+    if (inside_point == state) {
+      // check: end of point
+      if (head == rt->point_suffix
+          || (head == rt->point_delimiter && rt->point_suffix == '\0')) {
+        state    = outside_point;
+        get_next = head == rt->point_suffix;
+
+        parse_number(number, p);
+        ensure_dimension(p);
+
+        ++p.pts.points;
+        ++d;
+
+        // assert(d == p.pts.dimensions);
+        if (d != p.pts.dimensions) {
+          return print_syntax_error(head, p);
+        }
+
         continue;
       }
-      c = std::strtod(b, &e);
-      out.coords.push_back(c);
-      ++d;
-      b = e;
-    }
-    // add point
-    if (d > 0) {
-      ++out.points;
+
+      // check: end of coordinate
+      if (head == rt->coord_delimiter) {
+        parse_number(number, p);
+        ++d;
+        continue;
+      }
+
+      number.push_back(head);
     }
 
-    // check: dimension match
-    if (out.dimensions == 0) {
-      out.dimensions = d;
-    } else if (out.dimensions != d) {
-      std::string m = "pointset contains points of variable dimensions violating the "
-                      "regular pointset condition. ";
-      m += "expected dimension = " + std::to_string(out.dimensions);
-      m += "read dimension = " + std::to_string(d);
-      m += "line = " + ln;
-      m += ".";
-      throw std::runtime_error(m);
+    if (outside_point == state) {
+      if (head == rt->point_prefix
+          || (head == rt->point_delimiter && rt->point_prefix == '\0')) {
+        state = inside_point;
+        d     = 0;
+        continue;
+      } else if (head == rt->set_suffix) {
+        state = outside_set;
+      } else if (rt->point_prefix == '\0' && p.pts.coords.empty()) {
+        state    = inside_point;
+        get_next = false;
+        d        = 0;
+        continue;
+      }
+    }
+
+    // start point set
+    if (outside_set == state) {
+      if (head == rt->set_prefix || rt->set_prefix == '\0') {
+        state    = outside_point;
+        get_next = rt->set_prefix != '\0';
+        continue;
+      } else if (head == rt->set_suffix) {
+        break;
+      }
     }
   }
 };
 
 i32 return_results(const program_param& rt, const problem_param& problem)
 {
+  // check: given domain boundary and actual read dimension
+  if (problem.pts.dimensions * 2 != problem.pts.domain_bound.size()) {
+    std::cerr << "error: specified domain boundary mismatches the dimension of the point "
+                 "set read from input. "
+              << "Point set dimension = " << problem.pts.dimensions
+              << ". Domain boundaries = " << problem.pts.domain_bound.size()
+              << " (should be " << (problem.pts.dimensions * 2) << ")." << std::endl;
+    return EXIT_FAILURE;
+  }
+
   putln(rt.os, "# coordinates of points:", !rt.silent);
   putln(rt.os, "# (coord_0 coord_1 ... coord_n):", !rt.silent);
   write_pointset(rt.os, problem.pts, rt.delimiter);
@@ -111,8 +227,70 @@ u1 parse_progargs(i32 argc, const i8** argv, program_param& rt)
         return argparse::err("missing delimiter value. Consider using -h or --help.");
       rt.delimiter = arg[++i][0];
 
+    } else if (s == "--domain-boundary") {
+      if (!argparse::argval(arg, i))
+        return argparse::err(
+          "missing domain boundary value. Consider using -h or --help.");
+
+      // a tuple of values is a special case of a pointset
+      std::stringstream is(arg[++i]);
+      rt.domain_boundary.clear();
+      read_pointset(is, rt.domain_boundary);
+
+      if (rt.domain_boundary.coords.empty())
+        return argparse::err("invalid argument: domain boundary tuple may not be empty.");
+
+    } else if (s == "--mathematica") {
+      rt.point_delimiter = ',';
+      rt.coord_delimiter = ',';
+      rt.point_prefix    = '{';
+      rt.point_suffix    = '}';
+      rt.set_prefix      = '{';
+      rt.set_suffix      = '}';
+
+    } else if (s == "--csv") {
+      rt.point_delimiter = '\n';
+      rt.coord_delimiter = ' ';
+      rt.point_prefix    = '\0';
+      rt.point_suffix    = '\0';
+      rt.set_prefix      = '\0';
+      rt.set_suffix      = '\0';
+
+    } else if (s == "--point-delimiter") {
+      if (!argparse::argval(arg, i))
+        return argparse::err(
+          "missing point-delimiter value. Consider using -h or --help.");
+      rt.point_delimiter = arg[++i][0];
+
+    } else if (s == "--coord-delimiter") {
+      if (!argparse::argval(arg, i))
+        return argparse::err(
+          "missing coord-delimiter value. Consider using -h or --help.");
+      rt.coord_delimiter = arg[++i][0];
+
+    } else if (s == "--point-prefix") {
+      if (!argparse::argval(arg, i))
+        return argparse::err("missing point-prefix value. Consider using -h or --help.");
+      rt.point_prefix = arg[++i][0];
+
+    } else if (s == "--point-suffix") {
+      if (!argparse::argval(arg, i))
+        return argparse::err("missing point-suffix value. Consider using -h or --help.");
+      rt.point_suffix = arg[++i][0];
+
+    } else if (s == "--set-prefix") {
+      if (!argparse::argval(arg, i))
+        return argparse::err("missing set-prefix value. Consider using -h or --help.");
+      rt.set_prefix = arg[++i][0];
+
+    } else if (s == "--set-suffix") {
+      if (!argparse::argval(arg, i))
+        return argparse::err("missing set-suffix value. Consider using -h or --help.");
+      rt.set_suffix = arg[++i][0];
+
     } else if (s == "--silent") {
       rt.silent = true;
+
     } else if (s == "--i") {
       if (++i == arg.size())
         return argparse::err("invalid argument: -i misses a mandatory parameter");
@@ -123,7 +301,10 @@ u1 parse_progargs(i32 argc, const i8** argv, program_param& rt)
       rt.output = arg[i];
     } else if (s == "-h" || s == "--help") {
       std::cout << "NAME: reads a matrix of points" << std::endl;
-      std::cout << "SYNOPSIS: [--i FILE] [--o FILE] [--delimiter=CHARACTER] [--silent]"
+      std::cout << "SYNOPSIS: [--i FILE] [--o FILE] --domain-boundary LIST_OF_NUMBERS "
+                   "[--mathematica] [--csv] [--point-delimiter] [--coord-delimiter] "
+                   "[--point-prefix] [--point-suffix] [--set-prefix] [--set-suffix] "
+                   "[--delimiter=CHARACTER] [--silent]"
                 << std::endl;
       return false;
     }
@@ -141,12 +322,19 @@ dptk::i32 main(dptk::i32 argc, const dptk::i8** argv)
   dptk::i32           r;
 
   // default configuration
-  rt.silent    = false;
-  rt.delimiter = ' ';
-  rt.input     = "-";
-  rt.output    = "-";
-  problem.rt   = &rt;
-  r            = EXIT_SUCCESS;
+  rt.silent          = false;
+  rt.delimiter       = ' ';
+  rt.input           = "-";
+  rt.output          = "-";
+  rt.point_delimiter = '\n';
+  rt.coord_delimiter = ' ';
+  rt.point_prefix    = '\0';
+  rt.point_suffix    = '\0';
+  rt.set_prefix      = '\0';
+  rt.set_suffix      = '\0';
+  rt.comment_prefix  = '#';
+  problem.rt         = &rt;
+  r                  = EXIT_SUCCESS;
 
   // parse arguments
   if (!dptk::parse_progargs(argc, argv, rt)) {
@@ -163,17 +351,20 @@ dptk::i32 main(dptk::i32 argc, const dptk::i8** argv)
   dptk::putparam(rt.os, "source", rt.input, !rt.silent);
 
   // iterate through pointset sequence
-  while (!rt.is->eof() && r == EXIT_SUCCESS) {
+  while (rt.is->good() && r == EXIT_SUCCESS) {
     // clear pointset
     problem.pts.clear();
 
     // retrieve point set
-    dptk::read_matrix(*rt.is, problem.pts);
+    dptk::read_matrix(*rt.is, problem);
 
     // skip empty point sets
     if (problem.pts.coords.empty()) {
       continue;
     }
+
+    // apply specified domain boundary
+    problem.pts.domain_bound = rt.domain_boundary.coords;
 
     // show result
     r = dptk::return_results(rt, problem);
--- src/adapter/writematrix/main.cpp
+++ src/adapter/writematrix/main.cpp
@@ -21,9 +21,14 @@ struct program_param
   std::istream* is;
   std::ostream* os;
   u1            silent;
-  i8            delimiter;
   u1            transpose;
   u1            del_use_ipts;
+  i8            point_delimiter;
+  i8            coord_delimiter;
+  i8            point_prefix;
+  i8            point_suffix;
+  i8            set_prefix;
+  i8            set_suffix;
 };
 
 struct problem_param
@@ -32,22 +37,36 @@ struct problem_param
   program_param* rt;
 };
 
+void feed_token(std::ostream* os, i8 token)
+{
+  if (token == '\0')
+    return;
+  *os << token;
+}
+
 void write_matrix(problem_param& p)
 {
-  prec* c;
+  prec*          c;
+  program_param* rt;
 
-  ensure_precision(p.rt->os, *c);
+  rt = p.rt;
 
+  ensure_precision(rt->os, *c);
+
+  feed_token(rt->os, rt->set_prefix);
   for (u64 i = 0; i < p.pts.size(); ++i) {
+    feed_token(rt->os, rt->point_prefix);
     c = p.pts.at(i, 0);
     for (u64 j = 0; j < p.pts.dimensions; ++j) {
       if (j > 0) {
-        *p.rt->os << p.rt->delimiter;
+        feed_token(rt->os, rt->coord_delimiter);
       }
-      *p.rt->os << c[j];
+      *rt->os << c[j];
     }
-    *p.rt->os << std::endl;
+    feed_token(rt->os, rt->point_suffix);
+    feed_token(rt->os, rt->point_delimiter);
   }
+  feed_token(rt->os, rt->set_suffix);
 };
 
 void transpose(problem_param& p)
@@ -81,10 +100,60 @@ u1 parse_progargs(i32 argc, const i8** argv, program_param& rt)
   for (u64 i = 0; i < arg.size(); ++i) {
     const std::string& s = arg[i];
 
-    if (s == "--delimiter") {
+    if (s == "--mathematica") {
+      rt.point_delimiter = ',';
+      rt.coord_delimiter = ',';
+      rt.point_prefix    = '{';
+      rt.point_suffix    = '}';
+      rt.set_prefix      = '{';
+      rt.set_suffix      = '}';
+      rt.del_use_ipts    = false;
+
+    } else if (s == "--csv") {
+      rt.point_delimiter = '\n';
+      rt.coord_delimiter = ' ';
+      rt.point_prefix    = '\0';
+      rt.point_suffix    = '\0';
+      rt.set_prefix      = '\0';
+      rt.set_suffix      = '\0';
+      rt.del_use_ipts    = false;
+
+    } else if (s == "--point-delimiter") {
       if (!argparse::argval(arg, i))
-        return argparse::err("missing delimiter value. Consider using -h or --help.");
-      rt.delimiter    = arg[++i][0];
+        return argparse::err(
+          "missing point-delimiter value. Consider using -h or --help.");
+      rt.point_delimiter = arg[++i][0];
+      rt.del_use_ipts    = false;
+
+    } else if (s == "--coord-delimiter") {
+      if (!argparse::argval(arg, i))
+        return argparse::err(
+          "missing coord-delimiter value. Consider using -h or --help.");
+      rt.coord_delimiter = arg[++i][0];
+      rt.del_use_ipts    = false;
+
+    } else if (s == "--point-prefix") {
+      if (!argparse::argval(arg, i))
+        return argparse::err("missing point-prefix value. Consider using -h or --help.");
+      rt.point_prefix = arg[++i][0];
+      rt.del_use_ipts = false;
+
+    } else if (s == "--point-suffix") {
+      if (!argparse::argval(arg, i))
+        return argparse::err("missing point-suffix value. Consider using -h or --help.");
+      rt.point_suffix = arg[++i][0];
+      rt.del_use_ipts = false;
+
+    } else if (s == "--set-prefix") {
+      if (!argparse::argval(arg, i))
+        return argparse::err("missing set-prefix value. Consider using -h or --help.");
+      rt.set_prefix   = arg[++i][0];
+      rt.del_use_ipts = false;
+
+    } else if (s == "--set-suffix") {
+      if (!argparse::argval(arg, i))
+        return argparse::err("missing set-suffix value. Consider using -h or --help.");
+      rt.set_suffix   = arg[++i][0];
       rt.del_use_ipts = false;
 
     } else if (s == "--transpose" || s == "-t") {
@@ -102,8 +171,10 @@ u1 parse_progargs(i32 argc, const i8** argv, program_param& rt)
       rt.output = arg[i];
     } else if (s == "-h" || s == "--help") {
       std::cout << "NAME: writes as a matrix of points" << std::endl;
-      std::cout << "SYNOPSIS: [-ts]  [--i  FILE]  [--o  FILE]  [--transpose]  "
-                   "[--delim‐iter=CHARACTER] [--silent]"
+      std::cout << "SYNOPSIS: [-ts] [--i FILE] [--o FILE] [--mathematica] [--csv] "
+                   "[--point-delimiter] [--coord-delimiter] [--point-prefix] "
+                   "[--point-suffix] [--set-prefix] [--set-suffix] [--transpose] "
+                   "[--delimiter=CHARACTER] [--silent]"
                 << std::endl;
       return false;
     }
@@ -122,14 +193,19 @@ dptk::i32 main(dptk::i32 argc, const dptk::i8** argv)
   dptk::ipointset_read_info ipts_inf;
 
   // default configuration
-  rt.silent       = false;
-  rt.delimiter    = ' ';
-  rt.del_use_ipts = true;
-  rt.transpose    = false;
-  rt.input        = "-";
-  rt.output       = "-";
-  problem.rt      = &rt;
-  r               = EXIT_SUCCESS;
+  rt.silent          = false;
+  rt.del_use_ipts    = true;
+  rt.transpose       = false;
+  rt.input           = "-";
+  rt.output          = "-";
+  rt.point_delimiter = '\n';
+  rt.coord_delimiter = ' ';
+  rt.point_prefix    = '\0';
+  rt.point_suffix    = '\0';
+  rt.set_prefix      = '\0';
+  rt.set_suffix      = '\0';
+  problem.rt         = &rt;
+  r                  = EXIT_SUCCESS;
 
   // parse arguments
   if (!dptk::parse_progargs(argc, argv, rt)) {
@@ -156,7 +232,7 @@ dptk::i32 main(dptk::i32 argc, const dptk::i8** argv)
       continue;
     }
 
-    dptk::forward_delimiter(rt.del_use_ipts, ipts_inf, rt.delimiter);
+    dptk::forward_delimiter(rt.del_use_ipts, ipts_inf, rt.coord_delimiter);
 
     // skip empty point sets
     if (problem.pts.coords.empty()) {
