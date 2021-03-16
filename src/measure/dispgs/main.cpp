8168af9fb686f1dd7467e5b8ad3fc3796092796b
Benjamin Sommer <benjaminsommer@mailbox.org>
2021-03-16 10:26:42 +0100

    disp-gs may stream graphs (--graph-layout)

--- src/measure/dispgs/main.cpp
+++ src/measure/dispgs/main.cpp
@@ -27,6 +27,7 @@ struct program_param
   u1            compute_disp;
   u1            compute_ndisp;
   u1            compute_boxcount;
+  u1            layout_graph;
 };
 
 struct problem_measures
@@ -202,7 +203,8 @@ void dispersion(dispersion_param& ga, problem_param* problem)
 };
 
 i32 return_results(const program_param&                       rt,
-                   const std::vector<dptk::problem_measures>& measures)
+                   const std::vector<dptk::problem_measures>& measures,
+                   const std::vector<dptk::problem_param>&    problems)
 {
   putparam(rt.os, "point set sequence size", measures.size(), !rt.silent);
 
@@ -215,6 +217,7 @@ i32 return_results(const program_param&                       rt,
     if (!rt.silent) {
       *rt.os << "# ";
       i8 ndel = '(';
+      put_header_column(rt.os, "argument", ndel, ',', rt.layout_graph);
       put_header_column(rt.os, "dispersion", ndel, ',', rt.compute_disp);
       put_header_column(rt.os, "n*dispersion", ndel, ',', rt.compute_ndisp);
       put_header_column(rt.os, "number of boxes", ndel, ',', rt.compute_boxcount);
@@ -224,6 +227,7 @@ i32 return_results(const program_param&                       rt,
     pointset pts;
 
     pts.clear();
+    pts.append_domain_bound(0, INFINITY, rt.layout_graph);
     pts.append_domain_bound(0, INFINITY, rt.compute_disp);
     pts.append_domain_bound(0, INFINITY, rt.compute_ndisp);
     pts.append_domain_bound(0, INFINITY, rt.compute_boxcount);
@@ -231,6 +235,14 @@ i32 return_results(const program_param&                       rt,
     write_pointset_header(rt.os, pts, rt.delimiter);
 
     for (u64 i = 0; i < measures.size(); ++i) {
+      if (rt.layout_graph) {
+        if (problems[i].pts.arguments.empty()) {
+          *rt.os << "0";
+        } else {
+          putsci(rt.os, problems[i].pts.arguments[0], 16);
+        }
+        *rt.os << rt.delimiter;
+      }
       if (rt.compute_disp) {
         putsci(rt.os, measures[i].disp, 16);
         if (rt.compute_ndisp || rt.compute_boxcount)
@@ -272,6 +284,8 @@ u1 parse_progargs(i32 argc, const i8** argv, program_param& rt)
       rt.compute_ndisp = true;
     } else if (s == "--count-boxes") {
       rt.compute_boxcount = true;
+    } else if (s == "--graph-layout") {
+      rt.layout_graph = true;
     } else if (s == "--silent") {
       rt.silent = true;
     } else if (s == "--i") {
@@ -285,7 +299,7 @@ u1 parse_progargs(i32 argc, const i8** argv, program_param& rt)
     } else if (s == "-h" || s == "--help") {
       std::cout << "NAME: compute dispersion using a grow&shrink algorithm" << std::endl;
       std::cout << "SYNOPSIS: [--i  FILE]  [--o  FILE]  [--disp]  [--ndisp]  "
-                   "[--count-boxes] [--silent]"
+                   "[--count-boxes] [--graph-layout] [--silent]"
                 << std::endl;
       return false;
     }
@@ -318,6 +332,7 @@ dptk::i32 main(dptk::i32 argc, const dptk::i8** argv)
   rt.compute_ndisp    = false;
   rt.delimiter        = ' ';
   rt.del_use_ipts     = true;
+  rt.layout_graph     = false;
   rt.silent           = false;
   rt.input            = "-";
   rt.output           = "-";
@@ -393,7 +408,7 @@ dptk::i32 main(dptk::i32 argc, const dptk::i8** argv)
   }
 
   // show result
-  r = dptk::return_results(rt, measures);
+  r = dptk::return_results(rt, measures, problems);
 
   // clean up (heap allocations)
   dptk::istream_close(rt.is);
